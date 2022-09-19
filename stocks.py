78020d59254e3cb51b25a966a1d9665c96bf3543
talesa365 <talesahowell7@gmail.com>
2022-09-19 19:27:10 -0400

    fixed a sentence

--- stocks.py
+++ stocks.py
@@ -8,14 +8,15 @@ def stock_purchases():
 
     # Given the prices above and a client's investment budget, how much stock can they buy?
     # 1.1 TODO: Ask the client's name (use the string: "What is your name? ") and save it into a variable
-
+    first_name = input(f'What is your name{first_name}')
     # 1.2 TODO: Ask the client how many dollars they would like to invest (use the string: "How much would you like to invest? $")
     # and save it into a variable
+    invest = input(f'How much would you like to invest?{int(invest)} $')
     # NOTE: When you use the `input` function to get user input, what do numbers get saved as?
 
     # 1.3 TODO: Uncomment the line below to ask the client which stock they're interested in.
     # NOTE: Take a look at how this input string prints out
-    # stock_name = input("\nWhich stock are you interested in? Enter the full name:\nAmazon\nApple\nFacebook\nGoogle\nMicrosoft\nStock Name: ")
+    stock_name = input("\nWhich stock are you interested in? Enter the full name:\nAmazon\nApple\nFacebook\nGoogle\nMicrosoft\nStock Name: ")
     
     # Now that you have all of the client info, you can check how much they can purchase of the stock
     # they're interested in.
